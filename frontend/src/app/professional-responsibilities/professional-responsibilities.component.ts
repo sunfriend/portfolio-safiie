ed597dfe0db323fe4525f2df3be75af16432e7a0
Yu Nak <38046503+sunfriend@users.noreply.github.com>
2025-03-20 20:32:47 -0500

    some images added/ navbar logic change

--- frontend/src/app/professional-responsibilities/professional-responsibilities.component.ts
+++ frontend/src/app/professional-responsibilities/professional-responsibilities.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { PortfolioContentComponent } from "../portfolio-content/portfolio-content.component";
 
 @Component({
   selector: 'app-professional-responsibilities',
-  imports: [],
+  imports: [PortfolioContentComponent],
   templateUrl: './professional-responsibilities.component.html',
   styleUrl: './professional-responsibilities.component.scss'
 })
 export class ProfessionalResponsibilitiesComponent {
+headerTitlePhrase: string|undefined;
+contentBlocks: string[] = [];
 
 }
--- frontend/src/app/racial-consciousness-and-reflection/racial-consciousness-and-reflection.component.ts
+++ frontend/src/app/racial-consciousness-and-reflection/racial-consciousness-and-reflection.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { PortfolioContentComponent } from "../portfolio-content/portfolio-content.component";
 
 @Component({
   selector: 'app-racial-consciousness-and-reflection',
-  imports: [],
+  imports: [PortfolioContentComponent],
   templateUrl: './racial-consciousness-and-reflection.component.html',
   styleUrl: './racial-consciousness-and-reflection.component.scss'
 })
 export class RacialConsciousnessAndReflectionComponent {
+contentBlocks: string[] =[];
+headerTitlePhrase: string|undefined;
 
 }
